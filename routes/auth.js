fc08a03ca58277f4ce949003ea54b4dc9ebceab6
piyushghosh017 <hellopiyush0017@gmail.com>
2022-01-14 20:06:55 +0530

    commited

--- routes/auth.js
+++ routes/auth.js
@@ -31,7 +31,7 @@ router.post("/register", async (req,res)=>{
 router.post("/login",async (req,res)=>{
     try{
         const user=await User.findOne({username:req.body.username});
-        !user && res.status(400).json("wrong credential");
+        if (!user) return res.status(400).json("wrong credential");
 
         const validated= await bcrypt.compare(req.body.password,user.password)
         !validated && res.status(400).json("wrong credential");
